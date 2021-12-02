510fd995b55979b355b81f2c5a6c746fb61d9a47
Jonathan Choi <jonchoi11@gmail.com>
2021-12-02 13:50:20 -0700

    change footer text

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/react';
 
 const Footer = () => (
   <Box textAlign="center" padding="5" color='gray.600' borderTop='1px' borderColor='gray.100'>
-    2021 Honey & Vinegar Real Estate
+    Honey & Vinegar Real Estate, Inc. 2021
   </Box>
 );
 export default Footer;
