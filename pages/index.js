73c364c0a96c32dd331e9ec88b94870e74d3dd8f
Jonathan Choi <jonchoi11@gmail.com>
2021-12-02 09:59:35 -0700

    add fetch api funtion in util folder

--- pages/index.js
+++ pages/index.js
@@ -48,6 +48,7 @@ export default function Home() {
         linkName="/search?purpose=for-rent"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
+      <Flex flexWrap="wrap">{/* fetch properties and map */}</Flex>
       <Banner
         purpose="Buy A Home"
         title1="Find, Buy & Own Your"
@@ -58,6 +59,7 @@ export default function Home() {
         linkName="/search?purpose=for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
+      {/* fetch properties and map */}
     </Box>
   );
 }
---
+++ utils/fetchApi.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const baseUrl = "https://bayut.p.rapidapi.com";
+
+export const fetchApi = async (url) => {
+  const { data } = await axios.get(url, {
+    headers: {
+      "x-rapidapi-host": "bayut.p.rapidapi.com",
+      "x-rapidapi-key": "1666920e64msh94ed237174ec49bp1b32aejsn075192e878a2",
+    },
+  });
+};
