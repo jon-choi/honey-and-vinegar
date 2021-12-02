6c042bca7ae4f49657e50ccd21d8e7c58c3f7d70
Jonathan Choi <jonchoi11@gmail.com>
2021-12-02 10:25:00 -0700

    create property component and import inside index

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
+import Property from "../components/Property";
 import { baseUrl, fetchApi } from "../utils/fetchApi";
 
 const Banner = ({
@@ -51,7 +52,11 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         linkName="/search?purpose=for-rent"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
-      <Flex flexWrap="wrap">{/* fetch properties and map */}</Flex>
+      <Flex flexWrap="wrap">
+        {propertiesForRent.map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
       <Banner
         purpose="Buy A Home"
         title1="Find, Buy & Own Your"
@@ -62,7 +67,9 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         linkName="/search?purpose=for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
-      {/* fetch properties and map */}
+      {propertiesForSale.map((property) => (
+        <Property property={property} key={property.id} />
+      ))}
     </Box>
   );
 }
